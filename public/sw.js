a4973faf45c13e171b2a23d6eb22ed6279a5baed
victor0451 <victor.m.longo@gmail.com>
2020-10-08 17:06:01 -0300

    Cambio banner sorteo dia de la madre

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "9d11ea7095b60a828f2db49ed1004ade"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "b542f4e1d8114a27a42e98aefc843bba"
+    "revision": "c5ed2c8132324af14506588951ef09fa"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
